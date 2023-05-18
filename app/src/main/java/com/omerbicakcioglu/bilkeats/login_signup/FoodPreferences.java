9b782ce974e357de457b955db7deb3601851d8f6
Yiğit Koşum <yigitkosum@hotmail.com>
2023-05-18 17:42:11 +0300

    Auto stash before merge of "master" and "origin/master"
    foodpreference

---
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/login_signup/FoodPreferences.java
@@ -0,0 +1,21 @@
+package com.omerbicakcioglu.bilkeats.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omerbicakcioglu.bilkeats.R;
+
+public class FoodPreferences extends AppCompatActivity {
+    String[] foodPreferenceList={"meatSoup", "vegetableSoup" ,"yogurtSoup" ,"legumeSoup", "chickenBased", "legumeBased", "redMeatBased","fishBased", "vegetableBased", "rice", "pasta", "noodle","salad", "chocolateDessert","syrupDessert", "milkDessert", "pastry", "yogurt", "pickles", "fruits"};
+    int[] foodPreferencePoints= new int[20];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_preferences);
+    }
+
+
+}
--- app/src/main/java/com/omerbicakcioglu/bilkeats/login_signup/Preferences.java
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/login_signup/Preferences.java
@@ -3,19 +3,52 @@ package com.omerbicakcioglu.bilkeats.login_signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.omerbicakcioglu.bilkeats.R;
 
 public class Preferences extends AppCompatActivity {
-    private boolean nutAlergy = false;
-    private boolean lacotese = false;
-    private boolean vegan = false;
+    private boolean isNutAllergic = false;
+    private boolean isLactoseIntolerant = false;
+    private boolean isVegan = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+
+
     }
+    public void ok(){
+
+        Switch veganSwitch = (Switch) findViewById(R.id.veganSwitch);
+        if(veganSwitch.isChecked()){
+            isVegan = true;
+        }
+        Switch lactose = (Switch) findViewById(R.id.lactose);
+        if(lactose.isChecked()){
+            isLactoseIntolerant = true;
+        }
+        Switch nutAllergy = (Switch) findViewById(R.id.nutAllergy);
+        if(nutAllergy.isChecked()){
+            isNutAllergic = true;
+        }
+
+    }
+    public boolean getVegan(){
+        return isVegan;
+    }
+    public boolean getLactose(){
+        return isLactoseIntolerant;
+    }
+    public boolean getNut(){
+        return isNutAllergic;
+    }
+
 
 
 }
