53961985af011a7680c265e7bc3b5bd447cbd4e5
Furkan Başıbüyük <furkan.basibuyuk@ug.bilkent.edu.tr>
2023-05-18 16:50:23 +0300

    preferences 2

--- app/src/main/java/com/omerbicakcioglu/bilkeats/login_signup/Preferences.java
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/login_signup/Preferences.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.omerbicakcioglu.bilkeats.R;
+
 public class Preferences extends AppCompatActivity {
+    private boolean nutAlergy = false;
+    private boolean lacotese = false;
+    private boolean vegan = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
     }
+
+
 }
