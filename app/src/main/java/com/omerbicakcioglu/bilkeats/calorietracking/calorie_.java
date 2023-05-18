e42b38455aeb94f85d4066f3670b29ca44672d3f
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2023-05-18 16:55:42 +0300

    Calorie Tracking Tasarımı

---
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/calorietracking/calorie_.java
@@ -0,0 +1,16 @@
+package com.omerbicakcioglu.bilkeats.calorietracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omerbicakcioglu.bilkeats.R;
+
+public class calorie_ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_tracking);
+    }
+}
