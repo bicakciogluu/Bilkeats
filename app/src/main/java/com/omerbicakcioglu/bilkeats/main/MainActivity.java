773851f84c30b519271d1b22d5e6aa980a2d1af0
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2023-05-20 01:35:45 +0300

    BottomNavBar

--- app/src/main/java/com/omerbicakcioglu/bilkeats/main/MainActivity.java
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/main/MainActivity.java
@@ -1,16 +1,24 @@
 package com.omerbicakcioglu.bilkeats.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.omerbicakcioglu.bilkeats.R;
 import com.omerbicakcioglu.bilkeats.TestActivity;
 import com.omerbicakcioglu.bilkeats.databinding.ActivityMainBinding;
 import com.omerbicakcioglu.bilkeats.lpd.LpdActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+ BottomNavigationView nav;
 
     private ActivityMainBinding binding;
     @Override
@@ -20,6 +28,38 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+
+        nav = findViewById(R.id.nav);
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.calorieTrack:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.piano:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+
+
+                return true;
+            }
+        });
 
     }
 
