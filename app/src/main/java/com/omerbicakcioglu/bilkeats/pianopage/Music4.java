aa2c68f67d6ca870c2b73564038aa2ccc1dca1ac
Sefa Emre Kavgacı <emre.kavgaci@ug.bilkent.edu.tr>
2023-05-20 16:59:16 +0300

    Piano page arrengements

---
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/pianopage/Music4.java
@@ -0,0 +1,14 @@
+package com.omerbicakcioglu.bilkeats.pianopage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Music4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music4);
+    }
+}
