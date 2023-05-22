87dd394aebe2e2fd4802baed54862f448529941c
Furkan Başıbüyük <furkan.basibuyuk@ug.bilkent.edu.tr>
2023-05-22 23:59:43 +0300

    Calorie Tracking

--- app/src/main/java/com/omerbicakcioglu/bilkeats/calorietracking/CalorieTrackingActivity.java
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/calorietracking/CalorieTrackingActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.omerbicakcioglu.bilkeats.databinding.ActivityCalorieTrackingBinding;
 
+import org.checkerframework.checker.units.qual.A;
+
 public class CalorieTrackingActivity extends AppCompatActivity {
     public int takeCarbon;
     public int takeProtein;
@@ -26,10 +28,30 @@ public class CalorieTrackingActivity extends AppCompatActivity {
         binding = ActivityCalorieTrackingBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        proteinCalorieRemain = 800;
-        carbonCalorieRemain = 800;
-        fatCalorieRemain = 800;
-        shouldSumCalorie = proteinCalorieRemain + carbonCalorieRemain + fatCalorieRemain;
+
+        String gender = "Male";
+        String activityString = "Ligth";
+        double weigth = 80;
+        double height = 180;
+        int age = 20;
+        double activity = 1.35;
+        if(activityString.equals("Ligth")) activity = 1.2;
+        else if (activityString.equals("Moderate")) activity = 1.35;
+        else if (activityString.equals("Active")) activity = 1.5;
+
+        if(gender.equals("Female"))
+            shouldSumCalorie = (int)((655.1 + 9.56 * weigth + 1.85 * height - 4.67 * age) * activity);
+        else if (gender.equals("Male"))
+            shouldSumCalorie = (int)((66.5 + 13.75 * weigth + 5 * height - 6.77 * age) * activity);
+
+
+        proteinCalorieRemain = (int)(shouldSumCalorie * 30.0/100);
+        carbonCalorieRemain = (int)(shouldSumCalorie * 45.0/100);;
+        fatCalorieRemain = (int)(shouldSumCalorie * 25.0/100);
+        while(shouldSumCalorie != proteinCalorieRemain + carbonCalorieRemain + fatCalorieRemain){
+            carbonCalorieRemain += 1;
+        }
+
         binding.shouldSome.setText("of " + shouldSumCalorie + " kcal");
         binding.remainCarb.setText("" + carbonCalorieRemain);
         binding.remainProt.setText("" + proteinCalorieRemain);
