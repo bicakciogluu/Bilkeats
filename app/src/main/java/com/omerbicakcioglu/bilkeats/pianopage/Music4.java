a48e4c107bb7f9c0ca42a13652333ee4252f227a
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2023-05-25 12:28:48 +0300

    Main Menu Tasarım

--- app/src/main/java/com/omerbicakcioglu/bilkeats/pianopage/Music4.java
+++ app/src/main/java/com/omerbicakcioglu/bilkeats/pianopage/Music4.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.omerbicakcioglu.bilkeats.R;
+
 public class Music4 extends AppCompatActivity {
 
     @Override
